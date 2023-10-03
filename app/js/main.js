878f734e92cef18d9259683e0ff3331bc3211caf
seregatotsamyi <iserg.mos.ls@gmail.com>
2023-10-03 18:28:07 +0500

    rating

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,35 @@
 window.onload = function () {
   "use strict";
+
+  //rating
+  let ratingBtns = document.querySelectorAll('.raiting__label')
+  let ratingInputs = document.querySelectorAll('.raiting__input')
+
+  let changeRating = (i) => {
+    let cls = ['_current', '_gold']
+    ratingBtns.forEach((item) => item.classList.remove(...cls))
+    ratingBtns[i].classList.add('_current')
+
+    for (let count = 0; count <= i; count++) {
+      let parent = ratingInputs[count].closest('.raiting__item')
+      parent.querySelector('.raiting__label').classList.add('_gold')
+    }
+    showAllForm(i + 1)
+  }
+
+  ratingBtns.forEach((item, i) => {
+    item.addEventListener('click', () => changeRating(i))
+  })
+  //rating
+
+  let showAllForm = (value) => {
+    console.log("Показываю форму " + value)
+  }
+
 };
 
 
-$(function(){
+$(function () {
   let width = $(window).width();
   let body = $('.body');
 });
