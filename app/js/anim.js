645e7bbd87299020b7690fe9ede8f2ce1ce36d9b
seregatotsamyi <iserg.mos.ls@gmail.com>
2023-10-04 13:28:30 +0500

    form animation + mob-menu

--- app/js/anim.js
+++ app/js/anim.js
@@ -1,14 +1,22 @@
-window.onload = function () {
+export let animation = () => {
   "use strict";
   const windowInnerWidth = window.innerWidth
 
   if (windowInnerWidth > 576) {
+    anime({
+      targets: '.form__btn-img._a',
+      translateY: -10,
+      // easing: 'easeInOutExpo',
+      duration: 1000,
+      loop: true,
+      direction: 'alternate'
+    });
 
     anime({
       targets: '.form__img_1',
       opacity: [0, 1],
     });
-    
+
     anime({
       targets: '.form__img_2',
       opacity: [0, 1],
@@ -32,10 +40,187 @@ window.onload = function () {
       duration: 1000
 
     });
+
+    let animItemFirst = anime({
+      targets: '.form__item-wrap:first-child',
+      opacity: [0, 1],
+      duration: 1400,
+      delay: 1600,
+    });
+    let animItemFirstLeft = anime({
+      targets: '.form__item-wrap:first-child .form__left',
+      opacity: [0, 1],
+      translateY: [40, 0],
+      duration: 1000,
+      delay: animItemFirst.delay,
+      easing: 'easeInOutExpo',
+    })
+    let animItemFirstImg = anime({
+      targets: '.form__item-wrap:first-child .form__img-item',
+      scale: [0, 1],
+      duration: 1000,
+      delay: animItemFirstLeft.delay + 300,
+      easing: 'easeInOutExpo',
+    })
+    let animItemFirstInput = anime({
+      targets: '.form__item-wrap:first-child .form__input',
+      opacity: [0, 1],
+      duration: 1000,
+      delay: animItemFirstImg.delay + 500,
+      easing: 'easeInOutExpo',
+    })
+    let animItemSecond = anime({
+      targets: '.form__item-wrap:nth-child(2)',
+      opacity: [0, 1],
+      duration: 1400,
+      delay: animItemFirstInput.delay + 100,
+    });
+    let animItemSecondLeft = anime({
+      targets: '.form__item-wrap:nth-child(2) .form__left',
+      opacity: [0, 1],
+      translateY: [40, 0],
+      duration: 1000,
+      delay: animItemSecond.delay,
+      easing: 'easeInOutExpo',
+    })
+    let animItemSecondImg = anime({
+      targets: '.form__item-wrap:nth-child(2) .form__img-item',
+      scale: [0, 1],
+      duration: 1000,
+      delay: animItemSecondLeft.delay + 300,
+      easing: 'easeInOutExpo',
+    })
+    let animItemSecondRating = anime({
+      targets: '.form__item-wrap:nth-child(2) .raiting__item',
+      opacity: [0, 1],
+      translateY: [40, 0],
+      duration: 1000,
+      delay: function (el, i, l) {
+        return animItemSecondImg.delay + 300 + i * 200;
+      },
+      easing: 'easeInOutExpo',
+    })
+    anime({
+      targets: '.footer',
+      opacity: [0, 1],
+      delay: animItemSecondRating.delay + 500
+    });
+
   }
+}
 
+export let animationReviews = () => {
+  "use strict";
+  const windowInnerWidth = window.innerWidth
 
+  if (windowInnerWidth > 576) {
+    let animItemReviews = anime({
+      targets: '.form__item-wrap[data-option]',
+      opacity: [0, 1],
+      duration: 1400,
+    });
+    let animItemReviewsLeft = anime({
+      targets: '.form__item-wrap[data-option] .form__left',
+      opacity: [0, 1],
+      translateY: [40, 0],
+      duration: 1000,
+      delay: animItemReviews.delay,
+      easing: 'easeInOutExpo',
+    })
+    let animItemReviewsImg = anime({
+      targets: '.form__item-wrap[data-option] .form__img-item',
+      scale: [0, 1],
+      duration: 1000,
+      delay: animItemReviewsLeft.delay + 300,
+      easing: 'easeInOutExpo',
+    })
+    let animItemReviewsInput = anime({
+      targets: '.form__item-wrap[data-option] .form__input',
+      opacity: [0, 1],
+      duration: 1000,
+      delay: animItemReviewsImg.delay + 500,
+      easing: 'easeInOutExpo',
+    })
+  }
+}
+export let animationEnd = () => {
+  "use strict";
+  const windowInnerWidth = window.innerWidth
 
+  if (windowInnerWidth > 576) {
+    let animItemEnd = anime({
+      targets: '._end-anim-1',
+      opacity: [0, 1],
+      duration: 1000,
+      delay: 1200
+    });
+    let animItemEndLeft = anime({
+      targets: '._end-anim-1 .form__left',
+      opacity: [0, 1],
+      translateY: [40, 0],
+      duration: 1000,
+      delay: animItemEnd.delay,
+      easing: 'easeInOutExpo',
+    })
+    let animItemEndImg = anime({
+      targets: '._end-anim-1 .form__img-item',
+      scale: [0, 1],
+      duration: 1000,
+      delay: animItemEndLeft.delay + 300,
+      easing: 'easeInOutExpo',
+    })
+    let animItemEndInput = anime({
+      targets: '._end-anim-1 .form__input',
+      opacity: [0, 1],
+      duration: 1000,
+      delay: animItemEndImg.delay + 500,
+      easing: 'easeInOutExpo',
+    })
+    let animItemEnd2 = anime({
+      targets: '._end-anim-2',
+      opacity: [0, 1],
+      duration: 1000,
+      delay: animItemEndInput.delay + 100
+    });
+    let animItemEnd2Left = anime({
+      targets: '._end-anim-2 .form__left',
+      opacity: [0, 1],
+      translateY: [40, 0],
+      duration: 1000,
+      delay: animItemEnd2.delay,
+      easing: 'easeInOutExpo',
+    })
+    let animItemEnd2Img = anime({
+      targets: '._end-anim-2 .form__img-item',
+      scale: [0, 1],
+      duration: 1000,
+      delay: animItemEnd2Left.delay + 300,
+      easing: 'easeInOutExpo',
+    })
+    let animItemEnd2Input = anime({
+      targets: '._end-anim-2 .new-project',
+      opacity: [0, 1],
+      duration: 1000,
+      delay: animItemEnd2Img.delay + 500,
+      easing: 'easeInOutExpo',
+    })
+    let animBtn = anime({
+      targets: '.form__btn',
+      opacity: [0, 1],
+      duration: 1000,
+      delay: animItemEnd2Input.delay + 500,
+      easing: 'easeInOutQuad',
+    })
+    anime({
+      targets: '.form__img_3',
+      opacity: [0, 1],
+      delay: animBtn.delay + 500
+    });
 
-
+    anime({
+      targets: '.form__img_4',
+      opacity: [0, 1],
+      delay: animBtn.delay + 500
+    });
+  }
 }
--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,26 @@
+import {
+  animation,
+  animationReviews,
+  animationEnd
+} from './anim.js';
+
 window.onload = function () {
+
   "use strict";
+
+  animation()
+
+  //rating-data
+  let valueObj = {
+    phone: false,
+    rating: false,
+    ratingOld: false,
+    review: false,
+    wishes: false,
+    newProject: false,
+    showAll: false
+  }
+  //rating-data
 
   //rating
   let ratingBtns = document.querySelectorAll('.raiting__label')
@@ -14,7 +35,10 @@ window.onload = function () {
       ratingBtns[count].classList.add('_gold')
     }
 
-    showAllForm(document.querySelector('input[name="raiting"]:checked').value)
+    valueObj.rating = document.querySelector('input[name="raiting"]:checked').value
+
+    showAllForm()
+
   }
 
   ratingInputs.forEach((item, i) => {
@@ -24,28 +48,241 @@ window.onload = function () {
 
 
   //new-project
-
   let newProjectInputs = document.querySelectorAll('.new-project__input')
   let newProjectLabels = document.querySelectorAll('.new-project__label')
 
   let updateProjectValue = (i) => {
-    newProjectLabels.forEach((item) => item.classList.remove('_checked'))
+    newProjectLabels.forEach((item) => {
+      item.classList.remove('_checked')
+      item.classList.remove('_error')
+    })
     newProjectLabels[i].classList.add('_checked')
+    valueObj.newProject = document.querySelector('input[name="new-project"]:checked').value
+    validateAll(false)
   }
 
   newProjectInputs.forEach((item, i) => {
-    item.addEventListener('click', () => updateProjectValue(i))
+    item.addEventListener('change', () => updateProjectValue(i))
   })
 
   //new-project
 
+  //inputs
+  let phoneInput = document.querySelector('.form__input[name="phone"]')
+  let reviewInput = document.querySelector('.form__input[name="reviews"]')
+  let reviewBadInput = document.querySelector('.form__input[name="reviews-bad"]')
+  let wishesInput = document.querySelector('.form__input[name="wishes"]')
 
-  //logic form
-  let showAllForm = (value) => {
-    console.log("Показываю форму " + value)
+  //inputs
+
+  //validate
+  let validatePhone = (value, option) => {
+    if (value.length === 16) {
+      valueObj.phone = true
+      if (option) {
+        phoneInput.classList.remove('_error')
+        phoneInput.classList.add('_complete')
+      }
+
+      showAllForm()
+    } else {
+      valueObj.phone = false
+      if (option) {
+        phoneInput.classList.remove('_complete')
+        phoneInput.classList.add('_error')
+      }
+
+    }
   }
+  let validateReview = (value, arr, option) => {
+    if (value.length > 3) {
+      valueObj.review = true
+      if (option) {
+        arr.classList.remove('_error')
+        arr.classList.add('_complete')
+      }
+
+    } else {
+      valueObj.review = false
+      if (option) {
+        arr.classList.add('_error')
+        arr.classList.remove('_complete')
+      }
+
+    }
+  }
+  let validateWishes = (value, option) => {
+    if (value.length > 3) {
+      valueObj.wishes = true
+      if (option) {
+        wishesInput.classList.remove('_error')
+        wishesInput.classList.add('_complete')
+      }
+
+    } else {
+      valueObj.wishes = false
+      if (option) {
+        wishesInput.classList.remove('_complete')
+        wishesInput.classList.add('_error')
+      }
+
+    }
+  }
+  let validateAll = (option) => {
+    let res = true
+    for (let key in valueObj) {
+      if (valueObj[key] === false) {
+        res = false
+        switch (key) {
+          case "review": {
+            validateReview(reviewInput.value, reviewInput, option)
+            validateReview(reviewBadInput.value, reviewBadInput, option)
+            break
+          }
+          case "wishes": {
+            validateWishes("", option)
+            break
+          }
+          case "newProject": {
+            if (document.querySelector('input[name="new-project"]:checked') === null) {
+              if (option) {
+                newProjectLabels.forEach((item) => {
+                  item.classList.remove('_checked')
+                  item.classList.add('_error')
+                })
+              }
+            }
+            break
+          }
+        }
+      }
+    }
+    if (res) {
+      $('.form__btn').addClass('_active')
+    } else {
+      $('.form__btn').removeClass('_active')
+    }
+    return res
+  }
+  //validate
+
   //logic form
 
+  phoneInput.addEventListener('change', () => {
+    validatePhone(phoneInput.value, true)
+    validateAll(false)
+  })
+  reviewInput.addEventListener('change', () => {
+    validateReview(reviewInput.value, reviewInput, true)
+    validateAll(false)
+  })
+  reviewBadInput.addEventListener('change', () => {
+    validateReview(reviewBadInput.value, reviewBadInput, true)
+    validateAll(false)
+  })
+  wishesInput.addEventListener('change', () => {
+    validateWishes(wishesInput.value, true)
+    validateAll(false)
+  })
+
+  let showAllForm = () => {
+    if (valueObj.phone && valueObj.rating && !valueObj.showAll) {
+      valueObj.showAll = true
+      valueObj.ratingOld = valueObj.rating
+
+      if (valueObj.rating > 3) {
+        $('.form__item-wrap[data-option="bed"]').hide()
+        $('.form__item-wrap[data-option="nice"]').fadeIn()
+      } else {
+        $('.form__item-wrap[data-option="nice"]').hide()
+        $('.form__item-wrap[data-option="bed"]').fadeIn()
+      }
+
+      animationReviews()
+
+      $('.form__item-wrap._end-anim').slideDown()
+      $('.form__btn').fadeIn()
+      animationEnd()
+    }
+
+    if (valueObj.phone && valueObj.rating && valueObj.showAll) {
+      if (valueObj.ratingOld > 3 && valueObj.rating <= 3) {
+        valueObj.ratingOld = valueObj.rating
+        $('.form__item-wrap[data-option="nice"]').hide()
+        $('.form__item-wrap[data-option="bed"]').fadeIn()
+        animationReviews()
+      }
+      if (valueObj.ratingOld <= 3 && valueObj.rating > 3) {
+        valueObj.ratingOld = valueObj.rating
+        $('.form__item-wrap[data-option="bed"]').hide()
+        $('.form__item-wrap[data-option="nice"]').fadeIn()
+        animationReviews()
+      }
+
+    }
+
+  }
+
+  $('#form').on('submit', function (event) {
+    event.preventDefault();
+
+    let formData = $(this).serialize();
+
+    if (validateAll(true)) {
+      alert("Отправка")
+    } else {
+      alert('Валидация')
+    }
+
+
+    // $.ajax({
+    //   url: 'mail.php',
+    //   method: 'POST',
+    //   data: formData,
+    //   dataType: 'json',
+    //   encoding: true,
+    //   success: response => {
+    //     if (response == 1) {
+    //       step = 5;
+    //       quizNext();
+    //       $('#quiz-form')[0].reset();
+    //       window.location.href = '/thanks-quiz.html';
+
+    //     } else {
+    //       alert('Произошла ошибка');
+    //     }
+    //   },
+    //   error: function (jqXHR, exception) {
+    //     if (jqXHR.status === 0) {
+    //       alert('Not connect. Verify Network.');
+    //     } else if (jqXHR.status == 404) {
+    //       alert('Requested page not found (404).');
+    //     } else if (jqXHR.status == 500) {
+    //       alert('Internal Server Error (500).');
+    //     } else if (exception === 'parsererror') {
+    //       alert('Requested JSON parse failed.');
+    //     } else if (exception === 'timeout') {
+    //       alert('Time out error.');
+    //     } else if (exception === 'abort') {
+    //       alert('Ajax request aborted.');
+    //     } else {
+    //       alert('Uncaught Error. ' + jqXHR.responseText);
+    //     }
+    //   }
+    // });
+  })
+  //logic form
+
+  //mob-menu
+  let body = $('.body');
+  let toggleMenuBtn = $("#toggle-menu")
+  let mobMenu = $('#mobMenu')
+  toggleMenuBtn.on('click', () => {
+    toggleMenuBtn.toggleClass('_active')
+    body.toggleClass('_fixed')
+    mobMenu.toggleClass('_open')
+  })
+  //mob-menu
 
 
 };
@@ -53,5 +290,5 @@ window.onload = function () {
 
 $(function () {
   let width = $(window).width();
-  let body = $('.body');
+
 });
